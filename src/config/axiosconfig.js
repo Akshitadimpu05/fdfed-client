7d8db05e45810876970b10eec0ae8fe8efba76cb
jram17 <jayaramv405001@gmail.com>
2025-05-06 16:02:22 +0530

    modification in the axios config url

--- src/config/axiosconfig.js
+++ src/config/axiosconfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-const BASE_URL = import.meta.env.BASE_URL;
-
+import env_variables from "../config/envconfig";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+//const BASE_URL = env_variables.VITE_SERVER_URL;
 export default axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
